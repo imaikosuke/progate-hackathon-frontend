f6ff28ebbcb6681f034650b68f5ca3a98d2ae390
imaikosuke <imaikosuke2@gmail.com>
2024-05-03 02:38:41 +0900

    ルート一覧のリストでクリックされた地点の緯度と経度をStateに保持

--- app/components/RootListItem.tsx
+++ app/components/RootListItem.tsx
@@ -1,18 +1,19 @@
-// components/RootListItem.tsx
-"use client";
+// progate-hackathon-frontend/app/components/RootListItem.tsx
+'use client';
 import React from 'react';
 
 interface RootListItemProps {
   number: number;
   address: string;
+  lat: number;
+  lng: number;
+  onSelectLocation: (location: { lat: number; lng: number }) => void;
 }
 
-const RootListItem: React.FC<RootListItemProps> = ({ number, address }) => {
+const RootListItem: React.FC<RootListItemProps> = ({ number, address, lat, lng, onSelectLocation }) => {
   return (
     <div
-      onClick={() => {
-        console.log(`Item ${number} clicked`);
-      }}
+      onClick={() => onSelectLocation({ lat, lng })}
       className="bg-white rounded-lg p-4 flex items-center space-x-4 cursor-pointer hover:bg-gray-200"
     >
       <span className="font-semibold">{number}</span>
--- app/layouts/RootList.tsx
+++ app/layouts/RootList.tsx
@@ -1,20 +1,26 @@
-import RootListItems from "../components/RootListItems";
+// progate-hackathon-frontend/app/layouts/RootList.tsx
+import React from 'react';
+import RootListItems from '../components/RootListItems';
 
 const data = [
-  { number: 1, address: "東京スカイツリー" },
-  { number: 2, address: "すみだ水族館" },
-  { number: 3, address: "雷門" },
-  { number: 4, address: "浅草文化観光センター" },
-  { number: 5, address: "江戸東京博物館" },
+  { number: 1, address: '東京スカイツリー', lat: 35.7100627, lng: 139.8107004 },
+  { number: 2, address: 'すみだ水族館', lat: 35.710318, lng: 139.810764 },
+  { number: 3, address: '雷門', lat: 35.711258, lng: 139.796736 },
+  { number: 4, address: '浅草文化観光センター', lat: 35.711742, lng: 139.796574 },
+  { number: 5, address: '江戸東京博物館', lat: 35.696312, lng: 139.792806 },
 ];
 
-const RootList = () => {
+interface RootListProps {
+  onSelectLocation: (location: { lat: number; lng: number }) => void;
+}
+
+const RootList: React.FC<RootListProps> = ({ onSelectLocation }) => {
   return (
     <div className="p-4">
       <h2 className="text-xl font-semibold">ルート一覧</h2>
-      <RootListItems items={data} />
+      <RootListItems items={data} onSelectLocation={onSelectLocation} />
     </div>
   );
-}
+};
 
 export default RootList;
