36597a55ed53331aac781d0c161803d602658306
kou7306 <kouta.1206.y@icloud.com>
2024-05-04 00:57:59 +0900

    cors

--- app/make-ranking/page.tsx
+++ app/make-ranking/page.tsx
@@ -1,12 +1,40 @@
-import React from "react";
+"use client";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 import Contaienr from "../components/dnd-kit/Cotainer";
+import axios from "axios";
 
-const page = () => {
-  return (
-    <div>
-      <Contaienr />
-    </div>
-  );
+const getID = async (lon: string, lat: string) => {
+  try {
+    const response = await axios.post("http://localhost:8000/get_narrow", {
+      data: {
+        longitude: lon,
+        latitude: lat,
+      },
+    });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 };
 
-export default page;
+const Page = () => {
+  const searchParams = useSearchParams();
+  const lon = searchParams.get("lon") as string;
+  const lat = searchParams.get("lat") as string;
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getID(lon, lat);
+      setData(result);
+    };
+    fetchData();
+  }, [lon, lat]);
+
+  return <div>{data && <Contaienr data={data} />}</div>;
+};
+
+export default Page;
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["placehold.jp"],
+    domains: ["pub-cbf08acd7c1b4f9d88dfeaf6e6f97284.r2.dev"],
   },
 };
 export default nextConfig;
