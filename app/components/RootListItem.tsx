c021a3f89f640cfba6ab8615ef5558f995d10df1
kou7306 <kouta.1206.y@icloud.com>
2024-05-04 21:11:05 +0900

    api-connect

--- app/components/RootListItem.tsx
+++ app/components/RootListItem.tsx
@@ -1,23 +1,30 @@
 // progate-hackathon-frontend/app/components/RootListItem.tsx
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
+import Image from "next/image";
 
 interface RootListItemProps {
-  number: number;
-  address: string;
+  order: number;
+  id: number;
   lat: number;
   lng: number;
   onSelectLocation: (location: { lat: number; lng: number }) => void;
 }
 
-const RootListItem: React.FC<RootListItemProps> = ({ number, address, lat, lng, onSelectLocation }) => {
+const RootListItem: React.FC<RootListItemProps> = ({
+  order,
+  id,
+  lat,
+  lng,
+  onSelectLocation,
+}) => {
   return (
     <div
       onClick={() => onSelectLocation({ lat, lng })}
       className="bg-white rounded-lg p-4 flex items-center space-x-4 cursor-pointer hover:bg-gray-200"
     >
-      <span className="font-semibold">{number}</span>
-      <span>{address}</span>
+      <span className="font-semibold">{order}</span>
+      <span>{id}</span>
     </div>
   );
 };
